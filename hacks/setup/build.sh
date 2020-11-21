bff3311f288670362259ec51c7ac3d3b68d9a560
Cory G. Latschkowski <dev@localhost>
2020-11-21 12:57:13 -0600

    update: build.sh to main

--- hacks/setup/build.sh
+++ hacks/setup/build.sh
@@ -7,13 +7,13 @@ WEBDAV_IMAGE=${WEBDAV_GIT_URL}/imagestream-webdav.yml
 WEBDAV_BUILD=${WEBDAV_GIT_URL}/buildconfig-webdav.yml
 
 CODE_SERVER_BUILD=../../openshift/build/build-code-server-base-centos.yml
-oc process -f ${CODE_SERVER_BUILD} -p BUILD_REF=dev | oc apply -f -
+oc process -f ${CODE_SERVER_BUILD} -p BUILD_REF=main | oc apply -f -
 
 CODE_SERVER_BUILD=../../openshift/build/build-code-server-docker-centos-patch.yml
 oc apply -f ${CODE_SERVER_BUILD}
 
 CODE_SERVER_BUILD=../../openshift/build/build-code-server-base-debian.yml
-oc process -f ${CODE_SERVER_BUILD} -p BUILD_REF=dev | oc apply -f -
+oc process -f ${CODE_SERVER_BUILD} -p BUILD_REF=main | oc apply -f -
 
 CODE_SERVER_BUILD=../../openshift/build/build-code-server-docker-debian-patch.yml
 oc apply -f ${CODE_SERVER_BUILD}
