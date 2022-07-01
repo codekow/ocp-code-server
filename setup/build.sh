7384f372b8c205a831f71f020eaae242ba0746d6
Cory Latschkowski <koree@redhat.com>
2022-07-01 10:35:56 -0500

    update: remove quotes

--- setup/build.sh
+++ setup/build.sh
@@ -5,18 +5,28 @@ LABEL=${LABEL:-"testing=true"}
 
 cd "$(dirname "$0")" || exit 1
 
-TEMPLATE=../openshift/build/build-code-server-ubi-template.yml
-oc process -f ${TEMPLATE} -p BUILD_REF="${BUILD_REF:-main}" -l "${LABEL}" | oc apply -f -
-
+build_codercom(){
 TEMPLATE=../openshift/build/build-code-server-base-codercom-template.yml
 oc process -f ${TEMPLATE} -p BUILD_REF="${BUILD_REF:-main}" -l "${LABEL}" | oc apply -f -
 
 TEMPLATE=../openshift/build/build-code-server-dockerfile-codercom-patch.yml
 oc apply -f ${TEMPLATE}
+}
 
+build_ubi(){
+TEMPLATE=../openshift/build/build-code-server-ubi-template.yml
+oc process -f ${TEMPLATE} -p BUILD_REF="${BUILD_REF:-main}" -l "${LABEL}" | oc apply -f -
+}
+
+build_webdev(){
 WEBDAV_GIT_URL=https://raw.githubusercontent.com/codekow/webdav/main/node/openshift/build
 WEBDAV_IMAGE=${WEBDAV_GIT_URL}/imagestream-webdav.yml
 WEBDAV_BUILD=${WEBDAV_GIT_URL}/buildconfig-webdav.yml
 
 curl -sL ${WEBDAV_IMAGE} | oc apply -f -
 curl -sL ${WEBDAV_BUILD} | oc apply -f -
+}
+
+build_codercom
+#build_ubi
+build_webdev
