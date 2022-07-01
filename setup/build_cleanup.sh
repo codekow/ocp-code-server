9199325977d2df1a364f421c4274e21fb5204356
Cory Latschkowski <koree@redhat.com>
2022-07-01 16:41:11 -0500

    update: scripts

--- setup/build_cleanup.sh
+++ setup/build_cleanup.sh
@@ -7,9 +7,9 @@ WEBDEV_LABEL='build=webdav-node'
 # list the current project and wait for user allow user to bail out
 oc project
 echo "Preparing to delete the following items:"
-oc get all,ImageStream,BuildConfig -l ${CODE_LABEL} --no-headers | awk '{print $1}'
-oc get all,ImageStream,BuildConfig -l ${WEBDEV_LABEL} --no-headers | awk '{print $1}'
+oc get all,imagestream,buildconfig -l ${CODE_LABEL} --no-headers | awk '{print $1}'
+oc get all,imagestream,buildconfig -l ${WEBDEV_LABEL} --no-headers | awk '{print $1}'
 sleep 6
 
-oc delete all,ImageStream,BuildConfig -l ${CODE_LABEL}
-oc delete all,ImageStream,BuildConfig -l ${WEBDEV_LABEL}
+oc delete all,imagestream,buildconfig -l ${CODE_LABEL}
+oc delete all,imagestream,buildconfig -l ${WEBDEV_LABEL}
--- setup/deploy_cleanup.sh
+++ setup/deploy_cleanup.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 # Update the application name to match the value of your deployment
-APPLICATION_NAME=custom-code-server
+CODE_LABEL='testing=true'
 
 # list the current project and wait for user allow user to bail out
 oc project
 echo "Preparing to delete the following items:"
-oc get all,deploymentconfig,pvc,route,service,secret -l app=${APPLICATION_NAME} --no-headers | awk '{print $1}'
+oc get deploymentconfig,pvc,route,service,secret -l ${CODE_LABEL} --no-headers | awk '{print $1}'
 sleep 6
 
-oc delete all,deploymentconfig,pvc,route,service,secret -l app=${APPLICATION_NAME}
+oc delete deploymentconfig,pvc,route,service,secret -l ${CODE_LABEL}
